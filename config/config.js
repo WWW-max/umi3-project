ead4d7876792a80e6d3ec062d42b2d1a88fb08f4
monster-wu <464711201@qq.com>
2023-02-28 23:43:58 +0800

    feat: 使用@umijs/plugin-layout插件完成项目视图骨架

--- config/config.js
+++ config/config.js
@@ -7,4 +7,11 @@ export default defineConfig({
   },
   routes,
   fastRefresh: {},
+  layout: { // 开启项目视图骨架配置
+    // 支持任何不需要 dom 的
+    // https://procomponents.ant.design/components/layout#prolayout
+    name: 'Ant Design',
+    locale: true,
+    layout: 'side',
+  },
 });
--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,12 @@
 export default [
-    { path: '/', component: '@/pages/index' },
-    { path: '/test', component: '@/pages/Test'}
+    { 
+        path: '/',
+        component: '@/pages/index',
+        name: '首页',
+    },
+    { 
+        path: '/test',
+        component: '@/pages/Test',
+        name: '测试页面',
+    }
 ]
