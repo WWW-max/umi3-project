ddcc15b8c954f6743e3d94891336f727ab945e8d
monster-wu <464711201@qq.com>
2023-03-01 23:26:31 +0800

    feat: 子路由子菜单

--- config/routes.js
+++ config/routes.js
@@ -8,5 +8,21 @@ export default [
         path: '/test',
         component: '@/pages/Test',
         name: '测试页面',
+    },
+    {
+        path: '/test2',
+        name: '菜单项',
+        routes: [
+            {
+                path: '/test2/test21',
+                name: 'test21',
+                component: '@/pages/Test2/Test21.jsx',
+            },
+            {
+                path: 'test2/test22',
+                name: 'test22',
+                component: '@/pages/Test2/Test22.jsx',
+            }
+        ]
     }
 ]
---
+++ src/pages/Test2/Test21.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Test21() {
+  return (
+    <div>Test21</div>
+  )
+}
