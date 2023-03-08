6a0816b79d54fb4e612e22b20907b87c95e8ab4c
monster-wu <464711201@qq.com>
2023-03-08 23:13:29 +0800

    feat: 使用mockjs增强mock数据能力

--- mock/stu.js
+++ mock/stu.js
@@ -1,6 +1,19 @@
+import mockjs from 'mockjs';
 export default {
-    'GET /classes/test': {
-        username: '张三丰',
-        score: 100
-    }
-}
+  'GET /classes/test': {
+    username: '张三丰',
+    score: 100,
+  },
+  'GET /classes/stu': mockjs.mock({
+    code: 200,
+    msg: '学生数据加载成功',
+    data: [
+      {
+        name: '@name',
+        score: '@Integer(50,100)',
+        city: '@city',
+        time: '@date',
+      },
+    ],
+  }),
+};
