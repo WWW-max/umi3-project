2c229cf332078f870b3382222649767dc9634779
monster-wu <464711201@qq.com>
2023-04-16 23:11:50 +0800

    refactor: 修改mock数据方式

--- mock/stu.js
+++ mock/stu.js
@@ -1,4 +1,13 @@
 import mockjs from 'mockjs';
+let dataList = [
+  {
+    objectId: '@id',
+    name: '@name',
+    score: '@Integer(50,100)',
+    city: '@city',
+    time: '@date',
+  },
+];
 export default {
   'GET /classes/test': {
     username: '张三丰',
@@ -7,14 +16,6 @@ export default {
   'GET /classes/stu': mockjs.mock({
     code: 200,
     msg: '学生数据加载成功',
-    data: [
-      {
-        objectId: '@id',
-        name: '@name',
-        score: '@Integer(50,100)',
-        city: '@city',
-        time: '@date',
-      },
-    ],
+    data: dataList,
   }),
 };
