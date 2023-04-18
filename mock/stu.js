ffd1d286d48b06bbb4559c16b66c5a2dccfe8855
monster-wu <464711201@qq.com>
2023-04-18 23:49:13 +0800

    feat: mock方式调整

--- mock/stu.js
+++ mock/stu.js
@@ -1,23 +1,23 @@
 import mockjs from 'mockjs';
-let dataList = [
-  {
-    objectId: '@id',
-    name: '@name',
-    score: '@Integer(50,100)',
-    city: '@city',
-    time: '@date',
-  },
-];
+let dataList = mockjs.mock({
+  code: 200,
+  msg: '学生数据加载成功',
+  data: [
+    {
+      objectId: '@id',
+      name: '@name',
+      score: '@Integer(50,100)',
+      city: '@city',
+      time: '@date',
+    },
+  ],
+});
 export default {
   'GET /classes/test': {
     username: '张三丰',
     score: 100,
   },
-  'GET /classes/stu': mockjs.mock({
-    code: 200,
-    msg: '学生数据加载成功',
-    data: dataList,
-  }),
+  'GET /classes/stu': dataList,
   'DELETE /classes/stu': (req, res) => {
     res.send({
       code: 200,
