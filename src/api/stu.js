ee0b651846bfad9fbd4f1c19bf37fe151a0aeced
monster-wu <464711201@qq.com>
2023-04-21 23:42:08 +0800

    feat: 删除api

--- src/api/stu.js
+++ src/api/stu.js
@@ -3,3 +3,9 @@ import { request } from 'umi';
 export const getStuData = () => {
   return request('/classes/stu');
 };
+
+export const stuDel = (id) => {
+  return request(`/classes/stu?id=${id}`, {
+    method: 'DELETE',
+  });
+};
